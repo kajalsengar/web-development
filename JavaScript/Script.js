2f8593b039082d92186043c82495bc97f1bd9c5c
kajal sengar <sengarkajal089@gmail.com>
2025-02-21 22:16:11 +0530

    new

---
+++ JavaScript/Script.js
@@ -0,0 +1,14 @@
+alert("Hello world....")
+
+console.log("code is running .....")
+console.log("code is also running .....")
+console.log("code is running like a wow .....")
+
+var a = prompt("enter your number")
+console.log("number is = " + a)
+var isTrue = confirm("are sure to leave this page")
+if (isTrue) {
+    console.log("leave this page ")
+} else {
+    console.log("donot leave this page")
+}
