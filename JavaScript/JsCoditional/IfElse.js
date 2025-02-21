a81a87165755d1ca9e2470650aa81220d78d92ad
kajal sengar <sengarkajal089@gmail.com>
2025-02-22 00:49:37 +0530

    if else

---
+++ JavaScript/JsCoditional/IfElse.js
@@ -0,0 +1,14 @@
+console.log("hello this is a conditional ")
+
+let age =17;
+let grace=2;
+
+if ((age+2)>18){
+    console.log("you can drive...")
+}
+else if(age==0){
+console.log("are you kidding..")
+}
+else{
+    console.log("you can not drive....")
+}
