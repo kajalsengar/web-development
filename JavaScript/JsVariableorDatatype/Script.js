ce477f37bc5097105efc3cb1eb77400dda4f27f3
kajal sengar <sengarkajal089@gmail.com>
2025-02-22 00:27:19 +0530

    Java Script Variable, datatype and object

---
+++ JavaScript/JsVariableorDatatype/Script.js
@@ -0,0 +1,36 @@
+console.log("Hey everyone radhe radhe... ")
+
+var a = 5;
+var b = 6;
+var c = "kanha";
+
+{
+    let a = 108;
+    console.log(a);
+}
+console.log(a)
+
+console.log(a + b);
+console.log(typeof a, typeof b, typeof c);
+
+let s ="radhe"
+let n =21;
+let f=2.11;
+let t=true;
+let u=undefined;
+let l=null;
+
+console.log(s, n, f ,t ,u ,l)
+console.log(typeof s, typeof n, typeof f ,typeof t ,typeof u ,typeof l );
+
+let k ={
+    name: "kajal",
+    "job role": 59
+}
+console.log(k)
+
+k.salary = "100crore"
+console.log(k)
+
+k.salary="500crore"
+console.log(k)
