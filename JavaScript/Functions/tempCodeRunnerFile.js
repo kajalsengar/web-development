aba7f466e584cff8239b7c0853869ba0b9b88b8a
kajal sengar <sengarkajal089@gmail.com>
2025-02-23 00:08:26 +0530

    Strings

--- JavaScript/Functions/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-function nice(name) {
-    console.log("Hey " + name + " you are nice.")
-    console.log("Hey " + name + " you are good.")
-    console.log("Hey " + name + " you are your dress is nice.")
-    console.log("Hey " + name + " you are looking good.")
-}
---
+++ JavaScript/Strings/String.js
@@ -0,0 +1,23 @@
+console.log("This is a string...")
+
+let a = "kajal";
+console.log(a)
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+// console.log(a[5]);
+
+console.log(a.length);
+
+let R_name ="Radha";
+let friend= "Vishakha";
+console.log("Her name is " + R_name+ " and her friend name is " +friend );
+
+let k= "Krishna";
+console.log(k.toUpperCase())
+console.log(k.slice(1,4));
+
+console.log(k.replace("na","n"));
+console.log(R_name.concat(k,"Prem"))
