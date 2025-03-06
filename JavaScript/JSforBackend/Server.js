d7fd56d3cdd66c9f24e617a578155723c5b199f9
kajal sengar <sengarkajal089@gmail.com>
2025-03-06 22:35:32 +0530

    npm

--- JavaScript/JSforBackend/Server.js
+++ JavaScript/JSforBackend/Server.js
@@ -1,1 +1,10 @@
 console.log("Hello World..")
+
+var slugify = require('slugify')
+
+let a = slugify('some string')
+console.log(a);
+
+
+const b = slugify('some string' , '_')
+console.log(b);
--- Project/Spotify/Script.js
+++ Project/Spotify/Script.js
@@ -1,1 +1,11 @@
 console.log("Let`s write some java script");
+
+
+async function main(){
+    let a = await fetch("http://127.0.0.1:3000/Project/Spotify/Songs")
+    let response= await a.text;
+    console.log(response)
+}
+
+
+main()
